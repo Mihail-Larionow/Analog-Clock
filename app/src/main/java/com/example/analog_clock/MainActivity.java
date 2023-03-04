168ec29f7185ee890012849aac6dbc391bee378f
michel <mihail.larionow2001@gmail.com>
2023-03-04 13:50:56 +0300

    Made a PagerView

--- app/src/main/java/com/example/analog_clock/MainActivity.java
+++ app/src/main/java/com/example/analog_clock/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.analog_clock;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.analog_clock.adapters.PagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager2) findViewById(R.id.pager);
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(pagerAdapter);
     }
 }
